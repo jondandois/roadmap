34a3cb15cc99a8ede2d374566c506f454cc6afa5
Jonathan Dandois <jon.dandois@gmail.com>
2019-04-16 08:32:48 -0400

    adding in date xaxis

--- src/app/roadmap/roadmap/roadmap.component.ts
+++ src/app/roadmap/roadmap/roadmap.component.ts
@@ -1,20 +1,23 @@
 import { Component, OnInit } from '@angular/core';
 import { Task } from 'src/app/shared/models/task';
 import { TaskService } from 'src/app/shared/services/task.service';
+import * as moment from 'moment';
 
 @Component({
   selector: 'app-roadmap',
   templateUrl: './roadmap.component.html',
-  styleUrls: ['./roadmap.component.css']
+  styleUrls: ['./roadmap.component.css'],
 })
 export class RoadmapComponent implements OnInit {
   tasks: Task[];
   roadMapDates = {
     minDate: new Date("2/1/2019"),
-    maxDate: new Date("6/1/2019"),
+    maxDate: new Date("10/31/2019"),
+    dateColumns: [],
     nDays: 0,
     nWeeks: 0,
-    rowCount: 1
+    rowCount: 1,
+    nCols: 0
   }
 
   constructor(private taskService: TaskService) {}
@@ -22,7 +25,6 @@ export class RoadmapComponent implements OnInit {
   ngOnInit() {
     this.getTasks();
     this.buildCalendar();
-
   }
 
   getTasks(): void {
@@ -30,12 +32,22 @@ export class RoadmapComponent implements OnInit {
       .subscribe(tasks => this.tasks = tasks);
   }
 
+  enumerateDaysBetweenDates(startDate, endDate): Date[] {
+    let dates = [];
+    let currDate = startDate.startOf('week');
+    let lastDate = endDate.startOf('week');
+    while(currDate.add(1, 'week').diff(lastDate) < 0) {
+      dates.push(currDate.clone().toDate())
+    }
+    return dates;
+  }
+
   buildCalendar(): void {
-    const diffTime = this.roadMapDates.maxDate.getTime() - this.roadMapDates.minDate.getTime();
-    const nDays = Math.ceil(diffTime / (1000 * 3600 * 24));
-    const nWeeks = nDays / 7;
-    this.roadMapDates.nDays = nDays;
-    this.roadMapDates.nWeeks = nWeeks;
-    console.log('N Days: ' + nWeeks)
+    let startDate = moment(this.roadMapDates.minDate);
+    let endDate = moment(this.roadMapDates.maxDate);
+    this.roadMapDates.nDays = endDate.diff(startDate, 'days');
+    this.roadMapDates.nWeeks = endDate.diff(startDate, 'weeks');
+    this.roadMapDates.dateColumns = this.enumerateDaysBetweenDates(startDate, endDate);
+    this.roadMapDates.nCols = this.roadMapDates.dateColumns.length
   }
 }
